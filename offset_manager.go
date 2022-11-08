06afe60d35a9451b6dddeb51650cc345598f5dce
Nguyen Phuong <phuongnguyen.ng3010@gmail.com>
2022-11-08 16:34:29 +0700

    fix: manual reset offset when startup

--- offset_manager.go
+++ offset_manager.go
@@ -548,6 +548,13 @@ func (pom *partitionOffsetManager) ResetOffset(offset int64, metadata string) {
 	pom.lock.Lock()
 	defer pom.lock.Unlock()
 
+	if pom.offset < 0 {
+		pom.offset = offset
+		pom.metadata = metadata
+		pom.dirty = true
+		return
+	}
+
 	if offset <= pom.offset {
 		pom.offset = offset
 		pom.metadata = metadata
